77facde370a87549042668d6c856e6d98b658238
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-03-10 11:28:24 +0100

    import firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+const config = {
+  apiKey: "AIzaSyCyFEMjmxH00nYZWQ4MMqzDNPK5n3EBPhc",
+  authDomain: "lista-curso-f7744.firebaseapp.com",
+  projectId: "lista-curso-f7744",
+  storageBucket: "lista-curso-f7744.appspot.com",
+  messagingSenderId: "278678780821",
+  appId: "1:278678780821:web:51b8dff9afbb2dece5549c",
+  measurementId: "G-2BRTHMD3Z4"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
