4c26f736f4a4827aa10f3a14c463fe905f10aeb1
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-03-11 11:23:50 +0100

    mostramos alumnos por pantalla

--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -11,15 +11,32 @@ export class ListadoComponent implements OnInit {
   nombre:string;
   apellido1:string;
   apellido2:string;
-
+  alumnosRef; 
+  alumnosArray = [];
+  
   constructor(private router: Router, private db: AngularFirestore) {
-    const alumnos = this.db.collection('alumnos').valueChanges();
-    alumnos.subscribe(console.log);
+    this.alumnosRef = this.db.collection('alumnos');
+    const alumnos = this.db.collection('alumnos').snapshotChanges();
+    alumnos.subscribe((res)=>{
+      this.alumnosArray = res
+    });
   }
 
   ngOnInit(): void {}
- crearAlumno(nombre, apellido1, apellido2?){
+  
+  crearAlumno(nombre, apellido1, apellido2?){
+   console.log('Nombre', nombre);
+   console.log('Apellido', apellido1);
+   console.log('Apellido', apellido2);
+  //  añadimos alumnos a la base de datos 
+   this.alumnosRef.add({nombre: nombre, apellido1: apellido1, apellido2:apellido2 })
+
    
+
+
+ }
+ eliminar(nombre){
+   console.log('Nombre', nombre);
 
  }
   salir() {
