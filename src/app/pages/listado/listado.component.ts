3d44af78cebc44d3d7151447edc5e4a09653d490
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-03-11 11:03:34 +0100

    agregamos html y conectacmos con ts

--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent implements OnInit {
+  nombre:string;
+  apellido1:string;
+  apellido2:string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private db: AngularFirestore) {
+    const alumnos = this.db.collection('alumnos').valueChanges();
+    alumnos.subscribe(console.log);
   }
 
+  ngOnInit(): void {}
+ crearAlumno(nombre, apellido1, apellido2?){
+   
+
+ }
+  salir() {
+    this.router.navigateByUrl('login');
+  }
 }
