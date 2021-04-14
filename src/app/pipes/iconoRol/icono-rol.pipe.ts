ee0c1bf28dab30195d6daa0c45327f491e004112
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-04-14 13:02:27 +0200

    pusimos icono en vez de texto en rol

--- src/app/pipes/iconoRol/icono-rol.pipe.ts
+++ src/app/pipes/iconoRol/icono-rol.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'iconoRol'
+  name: 'iconoRol',
 })
 export class IconoRolPipe implements PipeTransform {
-
   transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (value === 'Jugador') {
+      return 'sports-basketball';
+    } else if (value === 'entrenador') {
+      return 'engineering';
+    } else if (value === 'arbitro') {
+      return 'sports';
+    } else {
+      return 'people-alt';
+    }
   }
-
 }
